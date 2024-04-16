f152411c22865f76c8b8ba5b178ba2f5cb42d312
Julien Fourdan <julien.fourdan.etu@univ-lemans.fr>
2024-04-16 13:57:30 +0200

    Add creation of extra information

--- src/main/java/explorateurIUT/services/mailManagement/MailContentForgerServiceImpl.java
+++ src/main/java/explorateurIUT/services/mailManagement/MailContentForgerServiceImpl.java
@@ -6,13 +6,25 @@ import jakarta.validation.ValidationException;
 
 @Service
 public class MailContentForgerServiceImpl implements MailContentForgerService {
+
+    private void createExtraInformation(MailSendingRequest mailSendingRequest, StringBuilder finalBody){
+        finalBody.append("Identité : ")
+        .append(mailSendingRequest.getContactIdentity())
+        .append("\n")
+        .append("Nom entreprise : ")
+        .append(mailSendingRequest.getContactCompany())
+        .append("\n")
+        .append("Fonction dans l'entreprise : ")
+        .append(mailSendingRequest.getContactFunction())
+        .append("\n")
+        .append("Mail du contact : ")
+        .append(mailSendingRequest.getContactMail());
+    }
     @Override
-    public String bodyCreation(String[] informations, String body) throws ValidationException{
-        String finalBody = body;
-        finalBody.concat("\n --------"); //Add a separator between the body and the informations
-        for(int i=0;i<informations.length;i++){
-            finalBody.concat("\n"+informations[i]);
-        }
-        return finalBody;
+    public String createBody(MailSendingRequest mailSendingRequest) throws ValidationException{
+        final StringBuilder finalBody = new StringBuilder(mailSendingRequest.getBody());
+        finalBody.append("\n").append("-".repeat(10)).append("\n"); //Add a separator between the body and the informations
+        this.createExtraInformation(mailSendingRequest, finalBody);
+        return finalBody.toString();
     }
 }
