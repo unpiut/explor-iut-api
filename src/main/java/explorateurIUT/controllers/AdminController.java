e5627d532ee67b4240ed9250744011911d242970
Julien Fourdan <julien.fourdan.etu@univ-lemans.fr>
2024-04-22 12:09:52 +0200

    Add get function adminController

--- src/main/java/explorateurIUT/controllers/AdminController.java
+++ src/main/java/explorateurIUT/controllers/AdminController.java
@@ -18,14 +18,23 @@
  */
 package explorateurIUT.controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import explorateurIUT.excelImport.AppDataProperties;
 
 /**
  *
@@ -37,9 +46,24 @@ public class AdminController {
 
     private static final Log LOG = LogFactory.getLog(AdminController.class);
 
+    private final AppDataProperties appDataProperties = new AppDataProperties();
+
     @GetMapping("data-sheets")
-    public String getData(@AuthenticationPrincipal UserDetails currentUser) {
-        return "T.B.A";
+    public ResponseEntity<?> getData(@AuthenticationPrincipal UserDetails currentUser) {
+        try (FileInputStream fis = new FileInputStream(new File(this.appDataProperties.getFilePath()))) {
+            ContentDisposition contentDisposition = ContentDisposition.builder("attachement") // doit être téléchargé
+                    .filename("data.xlsx") // le nom du fichier
+                    .build();
+            HttpHeaders contentHeaders = new HttpHeaders();
+            contentHeaders.setContentDisposition(contentDisposition);
+
+            return ResponseEntity.ok() // ok() : code 200
+                    .contentType(MediaType.valueOf("application/vnd.ms-excel")) // en-tête content-type positioné sur
+                                                                                // "application/vnd.ms-excel » : un
+                                                                                // fichier excel
+                    .headers(contentHeaders)
+                    .body(fis);
+        }
     }
 
     @PutMapping("data-sheets")
