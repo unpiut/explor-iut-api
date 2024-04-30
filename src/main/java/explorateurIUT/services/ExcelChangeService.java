5d53e0665296e9445ae50aac0f56cad9c9594ce2
Julien Fourdan <julien.fourdan.etu@univ-lemans.fr>
2024-04-30 15:15:52 +0200

    Try to make transactional service

--- src/main/java/explorateurIUT/services/ExcelChangeService.java
+++ src/main/java/explorateurIUT/services/ExcelChangeService.java
@@ -1,9 +1,13 @@
 package explorateurIUT.services;
 
 import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.web.multipart.MultipartFile;
 
+@Transactional
 public interface ExcelChangeService {
-    public void changeExcel(MultipartFile file) throws IOException;
+    public void changeExcel(InputStream dataExcel) throws IOException;
 }
--- src/main/java/explorateurIUT/services/ExcelChangeServiceImpl.java
+++ src/main/java/explorateurIUT/services/ExcelChangeServiceImpl.java
@@ -1,10 +1,47 @@
 package explorateurIUT.services;
 
+import explorateurIUT.excelImport.AppDataProperties;
 import java.io.IOException;
-import org.springframework.web.multipart.MultipartFile;
+import java.io.InputStream;
+import java.nio.file.AtomicMoveNotSupportedException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public class ExcelChangeServiceImpl implements ExcelChangeService {
-    public void changeExcel(MultipartFile file) throws IOException{
-        
+
+    private static final Log LOG = LogFactory.getLog(ExcelChangeServiceImpl.class);
+    
+    private final AppDataProperties appDataProperties;
+
+    @Autowired
+    public ExcelChangeServiceImpl(AppDataProperties appDataProperties) {
+        this.appDataProperties = appDataProperties;
+    }
+
+    @Override
+    public void changeExcel(InputStream dataExcel) throws IOException {
+        try {
+            Files.copy(Path.of(this.appDataProperties.getFilePath()), Path.of("../../resources/backupFile"), StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(dataExcel, Path.of(this.appDataProperties.getFilePath()), StandardCopyOption.REPLACE_EXISTING);
+        } catch  {
+            if (Files.exists(Path.of("../../resources/backupFile"))) {
+                if (Files.isSameFile(Path.of(this.appDataProperties.getFilePath()), Path.of("../../resources/backupFile"))) {
+                    try{
+                        Files.move(Path.of(this.appDataProperties.getFilePath()), Path.of("../../resources/backupFile"), StandardCopyOption.ATOMIC_MOVE);
+                    } catch(AtomicMoveNotSupportedException error){
+                        LOG.debug("The atomic move failed.");
+                        try{
+                        Files.move(Path.of(this.appDataProperties.getFilePath()), Path.of("../../resources/backupFile"), StandardCopyOption.REPLACE_EXISTING);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
