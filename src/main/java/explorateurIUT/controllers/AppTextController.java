5c1114ce7a7bec0d3edcf8ef0b1c7cd6a770fb46
Rémi Venant <remi.venant@univ-lemans.fr>
2024-09-05 08:16:40 +0200

    Fix missing annotatiion in rest endpoint method in AppTextController

--- src/main/java/explorateurIUT/controllers/AppTextController.java
+++ src/main/java/explorateurIUT/controllers/AppTextController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import explorateurIUT.services.AppTextService;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/v1/textes")
@@ -19,6 +20,7 @@ public class AppTextController {
         this.appTextSvc = appTextService;
     }
 
+    @GetMapping
     public Map<String, String> getText(@RequestParam(name="lang", defaultValue = "fr", required = false) String language) {
         return this.appTextSvc.getAppTextsByCode(language);
     }
