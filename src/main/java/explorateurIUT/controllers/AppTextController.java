c3f2a91f8e6cf682419a6e8a53b9be92930a0c92
Rémi Venant <remi.venant@univ-lemans.fr>
2024-09-05 08:20:16 +0200

    polish and add endpoint to retrieve a single text.

--- src/main/java/explorateurIUT/controllers/AppTextController.java
+++ src/main/java/explorateurIUT/controllers/AppTextController.java
@@ -8,20 +8,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import explorateurIUT.services.AppTextService;
+import java.util.NoSuchElementException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/textes")
 public class AppTextController {
+
     private final AppTextService appTextSvc;
 
     @Autowired
-    public AppTextController(AppTextService appTextService){
+    public AppTextController(AppTextService appTextService) {
         this.appTextSvc = appTextService;
     }
 
     @GetMapping
-    public Map<String, String> getText(@RequestParam(name="lang", defaultValue = "fr", required = false) String language) {
+    public Map<String, String> getAllTexts(@RequestParam(name = "lang", defaultValue = "fr", required = false) String language) {
         return this.appTextSvc.getAppTextsByCode(language);
+    }
+
+    @GetMapping("{textId:[a-zA-Z0-9_\\-]{2,100}}")
+    public String getText(
+            @PathVariable String textId,
+            @RequestParam(name = "lang", defaultValue = "fr", required = false) String language) {
+        final String text = this.appTextSvc.getAppTextsByCode(language).get(textId);
+        if (text == null) {
+            throw new NoSuchElementException("Unknown text");
+        }
+        return text;
     }
 }
